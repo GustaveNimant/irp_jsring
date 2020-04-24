a47db54244ec6fec064b09371b63dd2e8f435751
Emile Achadde <achadde@iabloko.fecamp>
2020-04-24 16:07:47 +0200

    checkValidOfKeyOfValue added

---
+++ RegExp.js
@@ -0,0 +1,12 @@
+	     let rex0 = /\$Author: ([a-zA-Z][a-zA-Z0-9]+(?:[\. ])?[a-zA-Z0-9_]+)\$/;
+	     let rex1 = /\$Date: (([0-3][0-9])\/([0-1][0-9])\/([0-9][0-9]))\$/;
+	     let rex2 = /\$mutable: ((\/(?:\.)?[a-z]+)+)\/([a-z]+\.[a-z]+)\$/;
+	     let rex3 = /\$next: (\w+)\$/;
+	     let rex4 = /\$parents: (Qm([a-zA-Z0-9]+))\$/;
+	     let rex5 = /\$previous: (Qm([a-zA-Z0-9]+))\$/;
+	     let rex6 = /\$qm: (z([a-zA-Z0-9]+))\$/;
+	     let rex7 = /\$Signature: ([a-zA-Z][a-zA-Z0-9]*(?:[\/])?[a-zA-Z0-9])\$/;
+	     let rex8 = /\$Source: ((\/[a-z]+)+)\/([a-z]+\.[a-z]+)(,[a-z])?\$/;
+	     let rex9 = /\$spot: (\d+)\$/;
+	     let rex10 = /\$tic: (\d{10})\$/;
+	     
