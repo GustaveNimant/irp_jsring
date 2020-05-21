bf6b64c6e304c59f6ac1aa34bc0128a1e64a4f2f
Emile Achadde <achadde@iabloko.fecamp>
2020-05-21 15:46:35 +0200

    ab_MfsOneDirOneFile.png ea_fun_MfsOneDirOneFile.png

--- caller_to_callee_to_dot.sh
+++ caller_to_callee_to_dot.sh
@@ -7,8 +7,11 @@ echo "cp $file to e.html"
 echo "insert_file_after.pl function caller_to_callee.txt e.html"
 echo "run e.html to get e.log"
 
+
+cut -d":" e.log -f1 > E.log
+
 echo "digraph dgn {" > caller_to_callee.dot
-sort -u e.log >> caller_to_callee.dot
+sort -u E.log >> caller_to_callee.dot
 echo "}" >> caller_to_callee.dot
 /keep/PERL/delete_lines_where.pl validate caller_to_callee.dot
 /keep/PERL/delete_lines_where.pl substi caller_to_callee.dot
