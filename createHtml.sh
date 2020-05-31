19b11ca316049851ddedc564419bf76dee9fa3ca
Emile Achadde <achadde@iabloko.fecamp>
2020-05-31 15:54:18 +0200

    Html Title added

--- createHtml.sh
+++ createHtml.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-cat html/head.html html/uploadBlock.html html/createDirectoryBlock.html html/removeBlock.html html/pinBlock.html html/currentMfsDirectoryBlock.html html/hiddenForDirBlock.html html/windowOnLoad.html html/tableFunctions.html html/reste.html > f.html
+cat \
+    html/headBlock.html \
+    html/uploadBlock.html \
+    html/createDirectoryBlock.html \
+    html/removeBlock.html \
+    html/pinBlock.html \
+    html/currentMfsDirectoryBlock.html \
+    html/hiddenForDirBlock.html \
+    html/windowOnLoad.html \
+    html/tableFunctions.html \
+    html/reste.html \
+    > f.html
+
+echo "cp f.html "
