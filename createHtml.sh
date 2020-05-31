828f2644cc0dbd04a8d1dddfb3205484b36c0f6e
Emile Achadde <achadde@iabloko.fecamp>
2020-05-31 13:33:52 +0200

    createHtml.sh ea_ModularVersionV0.html

---
+++ createHtml.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat html/head.html html/uploadBlock.html html/createDirectoryBlock.html html/removeBlock.html html/pinBlock.html html/currentMfsDirectoryBlock.html html/hiddenForDirBlock.html html/windowOnLoad.html html/tableFunctions.html html/reste.html > f.html
