3bbf4f3c4a55a1b8f8a1c6d287d7454598a309e6
Emile Achadde <achadde@iabloko.fecamp>
2020-06-15 18:16:54 +0200

    uploadBlock.html switch

--- code/functionScript.js
+++ code/functionScript.js
@@ -269,44 +269,6 @@ function valueInputTextOfId(id) {
     return result;
 }
 
-function valueOfId(id) {
-    let [callee, caller] = functionNameJS();
-    console.log('Entering in',callee,'called by',caller);
-    console.log(callee+'.input.id:',id);
-
-    let doc = document.getElementById(id);
-    console.log(callee+'.doc:',doc);
-
-    let tagNam = doc.tagName;
-    console.log(callee+'.tagNam:',tagNam);
-    let result;
-    switch (tagNam) {
-    case "INPUT":
-	if (doc.type == "text") {
-	    result = doc.value;
-	}
-	else {
-	    console.error(callee+'.tagNam:',tagNam,'type',doc.type);
-	    throw "unknown Type "+doc.type+" for TagName "+tagNam;	
-	}
-	break;
-    case "TABLE":
-	result = doc.innerHTML;
-	break;
-    case "TEXTAREA":
-	result = doc.value;
-	break;
-    case "SPAN":
-	result = doc.innerHTML;
-	break;
-    default:
-	console.error(callee+'.tagNam:',tagNam);
-	throw "unknown TagName "+tagNam;	
-    }
-    console.log(callee+'.result:',result);
-    return result;
-}
-
 function innerHTMLSpanOfId(id) {
     let [callee, caller] = functionNameJS();
     console.log('Entering in',callee,'called by',caller);
@@ -369,4 +331,96 @@ function typeImageOfJsonOfKey (json, key) {
     console.log (callee+'.result:', result);
     return result;
 }
+
+function nameSwitchOfId(id) {
+    let [callee, caller] = functionNameJS();
+    console.log('Entering in',callee,'called by',caller);
+    console.log(callee+'.input.id:',id);
+
+    let doc = document.getElementById(id);
+    console.log(callee+'.doc:',doc);
+
+    let tagNam = doc.tagName;
+    console.log(callee+'.tagNam:',tagNam);
+    
+    var result;
+    switch (tagNam) {
+    case "BUTTON":
+	if (doc.type == "submit") {
+	    result = doc.name;
+	}
+	else {
+	    console.error(callee+'.tagNam:',tagNam,'type',doc.type);
+	    throw "unknown Type "+doc.type+" for TagName "+tagNam;	
+	}
+	break;
+    case "INPUT":
+	if ((doc.type == "button") || (doc.type == "text")) {
+	    result = doc.name;
+	}
+	else {
+	    console.error(callee+'.tagNam:',tagNam,'type',doc.type);
+	    throw "unknown Type "+doc.type+" for TagName "+tagNam;	
+	}
+	break;
+    case "FORM":
+	result = doc.name;
+	break;
+    default:
+	console.error(callee+'.tagNam:',tagNam);
+	throw "No name for TagName "+tagNam;	
+    }
+    console.log(callee+'.result:',result);
+    return result;
+}
+
+function valueSwitchOfId(id) {
+    let [callee, caller] = functionNameJS();
+    console.log('Entering in',callee,'called by',caller);
+    console.log(callee+'.input.id:',id);
+
+    let doc = document.getElementById(id);
+    console.log(callee+'.doc:',doc);
+
+    let tagNam = doc.tagName;
+    console.log(callee+'.tagNam:',tagNam);
+    
+    var result;
+    switch (tagNam) {
+    case "BUTTON":
+	if (doc.type == "submit") {
+	    result = doc.value;
+	} else {
+	    console.error(callee+'.tagNam:',tagNam,'type',doc.type);
+	    throw "unknown Type "+doc.type+" for TagName "+tagNam;
+	}
+	break;
+    case "DIV":
+	result = doc.innerText;
+	break;
+    case "INPUT":
+	if ((doc.type == "button") || (doc.type == "text")){ 
+	    result = doc.value;
+	}
+	else {
+	    console.error(callee+'.tagNam:',tagNam,'type',doc.type);
+	    throw "unknown Type "+doc.type+" for TagName "+tagNam;	
+	}
+	break;
+    case "FORM":
+    case "SPAN":
+    case "TABLE":
+	result = doc.innerHTML;
+	break;
+    case "TEXTAREA":
+	result = doc.value;
+	break;
+    default:
+	console.error(callee+'.tagNam:',tagNam);
+	throw "No value for TagName "+tagNam;	
+    }
+    console.log(callee+'.result:',result);
+    return result;
+}
+
 
