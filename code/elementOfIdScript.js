1ebc0b1a373be3eadbeb8d082c7b459f40cfa65a
Emile Achadde <achadde@iabloko.fecamp>
2020-06-16 17:31:24 +0200

    elementOfIdOfTagOfType added

--- code/elementOfIdScript.js
+++ code/elementOfIdScript.js
@@ -19,6 +19,7 @@ function elementOfIdOfTag (eleId, eleTag ) {
     let [callee, caller] = functionNameJS();
     console.log('Entering in',callee,'called by',caller);
     console.log(callee+'.input.eleId:',eleId);
+    console.log(callee+'.input.eleTag:',eleTag);
     
     let element = document.getElementById(eleId);
     console.log(callee+'.element:',element);
@@ -28,6 +29,32 @@ function elementOfIdOfTag (eleId, eleTag ) {
     
     if(tagNam != eleTag) {
 	errorMessage (eleTag, tagNam, "check Id "+eleId, caller);
+    }
+    return element;    
+}
+
+function elementOfIdOfTagOfType (eleId, eleTag, eleTyp ) {
+    let [callee, caller] = functionNameJS();
+    console.log('Entering in',callee,'called by',caller);
+    console.log(callee+'.input.eleId:',eleId);
+    console.log(callee+'.input.eleTag:',eleTag);
+    console.log(callee+'.input.eleTyp:',eleTyp);
+    
+    let element = document.getElementById(eleId);
+    console.log(callee+'.element:',element);
+
+    let tagNam = element.tagName;
+    console.log(callee+'.tagNam:',tagNam);
+    
+    if(tagNam != eleTag) {
+	errorMessage (eleTag, tagNam, "check Id "+eleId, caller);
+    }
+
+    let typNam = element.type;
+    console.log(callee+'.typNam:',typNam);
+    
+    if(typNam != eleTyp) {
+	errorMessage (eleTyp, typNam, "check Id "+eleId+" check Tag "+eleTag, caller);
     }
     return element;    
 }
